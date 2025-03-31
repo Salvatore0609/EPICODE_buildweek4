cec217a72071f2945584e373fbea442c9d2cba12
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-03-31 12:18:00 +0200

    ciao3

--- BuildWeek4/src/main/java/it/epicode/progetto/coseACaso.java
+++ BuildWeek4/src/main/java/it/epicode/progetto/coseACaso.java
@@ -4,5 +4,6 @@ public class coseACaso {
     public static void main(String[] args) {
         System.out.println("ciao");
         System.out.println("ciao2");
+        System.out.println("ciao3");
     }
 }
