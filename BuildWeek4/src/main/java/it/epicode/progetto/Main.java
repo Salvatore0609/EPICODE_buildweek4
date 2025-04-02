42ecba26b319f4af90db8a6e3c1c39bb7b4e45d1
Timoteo Capurro <timoteo.capurro@infosyscap.net>
2025-04-02 11:04:56 +0200

    registrazione nuovo utente

--- BuildWeek4/src/main/java/it/epicode/progetto/Main.java
+++ BuildWeek4/src/main/java/it/epicode/progetto/Main.java
@@ -9,26 +9,30 @@ import static it.epicode.progetto.utility.Input.scanner;
 
 public class Main {
     public static void main(String[] args) {
-        ClearTerminal.clearConsole();
-        System.out.println("****************************");
-        System.out.println("***** MENU PRINCIPALE ******");
-        System.out.println("****************************");
-        System.out.println();
-        System.out.println("1. Login");
-        System.out.println("2. Registra un nuovo utente");
-        System.out.println("3. Acquista un biglietto");
-        System.out.println("0. Esci");
-        System.out.println();
-        int scelta = scanner.nextInt();
-        scanner.nextLine();
-        if (scelta == 2) {
-            GestioneUtenti.crea();
-        } else if (scelta == 1) {
-            Login login = new Login();
-            login.login();
-        } else if (scelta == 0) {
-            System.out.println("Arrivederci!");
-            System.exit(0);
+        while (true) {
+            ClearTerminal.clearConsole();
+            System.out.println("****************************");
+            System.out.println("***** MENU PRINCIPALE ******");
+            System.out.println("****************************");
+            System.out.println();
+            System.out.println("1. Login");
+            System.out.println("2. Registra un nuovo utente");
+            System.out.println("3. Acquista un biglietto");
+            System.out.println("0. Esci");
+            System.out.println();
+            int scelta = scanner.nextInt();
+            scanner.nextLine();
+            if (scelta == 2) {
+                boolean isAdmin = false;
+                GestioneUtenti.crea(isAdmin);
+            } else if (scelta == 1) {
+                Login login = new Login();
+                login.login();
+            } else if (scelta == 0) {
+                System.out.println("Arrivederci!");
+                return;
+            }
         }
+
     }
 }
